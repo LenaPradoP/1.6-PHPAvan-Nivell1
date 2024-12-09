9134689eb6b57ed9ca7eabe47b8aeb4adf1f1665
LenaPradoP <lena@atajo.es>
2024-12-09 12:42:05 +0100

    Ejercicio 1

---
+++ action.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+if (isset($_POST['username']) && isset($_POST['email'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+}
+
+echo "Username: $username<br>";
+echo "Email: $email <br>";
+echo "<br>";
+
+$_SESSION['username'] = $username;
+$_SESSION['email'] = $email;
+
+echo "Variables de sesión: <br>";
+echo $_SESSION['username'] . "<br>";
+echo $_SESSION['email'] . "<br>";
+?>
